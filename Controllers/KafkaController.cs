a1ee88838923f15c5ef2c8005ea3be02e78d1a56
josemita0301 <josemielmer2002@hotmail.com>
2025-01-29 00:42:06 -0500

    Api

---
+++ Controllers/KafkaController.cs
@@ -0,0 +1,49 @@
+﻿using IntegracionPubSub.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IntegracionPubSub.Controllers
+{
+    public class KafkaController : Controller
+    {
+        private readonly KafkaProducerService _producer;
+
+        public KafkaController(KafkaProducerService producer)
+        {
+            _producer = producer;
+        }
+
+        [HttpPost("publicar-topic-1")]
+        public async Task<IActionResult> PublicarMensajeTopic1([FromBody] string mensaje)
+        {
+            try
+            {
+                string topic1 = "topicApp1";
+                await _producer.SendMessageAsync(mensaje, topic1);
+                return Ok("📩 Mensaje enviado a Kafka.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                throw;
+            }
+            
+        }
+
+        [HttpPost("publicar-topic-2")]
+        public async Task<IActionResult> PublicarMensajeTopic2([FromBody] string mensaje)
+        {
+            try
+            {
+                string topic2 = "topicApp2";
+                await _producer.SendMessageAsync(mensaje, topic2);
+                return Ok("📩 Mensaje enviado a Kafka.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                throw;
+            }
+
+        }
+    }
+}
---
+++ Services/KafkaProducerService.cs
@@ -0,0 +1,29 @@
+﻿using Confluent.Kafka;
+
+namespace IntegracionPubSub.Services
+{
+    public class KafkaProducerService
+    {
+        private readonly string _bootstrapServers = "localhost:9092"; // Dirección de Kafka
+
+        public async Task SendMessageAsync(string message, string topic)
+        {
+            var config = new ProducerConfig
+            {
+                BootstrapServers = _bootstrapServers,
+                Debug = "broker,protocol"
+            };
+
+            using var producer = new ProducerBuilder<Null, string>(config).Build();
+            try
+            {
+                var result = await producer.ProduceAsync(topic, new Message<Null, string> { Value = message });
+                Console.WriteLine($"✅ Mensaje enviado: {result.TopicPartitionOffset}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"❌ Error enviando mensaje: {ex.Message}");
+            }
+        }
+    }
+}
